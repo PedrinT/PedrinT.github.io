c429d9b8e8537271815a11032aca2453fa86acde
Pedro T <pedrohtbernabe@gmail.com>
2023-07-17 17:40:34 -0300

    update

---
+++ scripts/meuCalendário.js
@@ -0,0 +1,27 @@
+
+var calendario = []
+var diaInicial = 1
+var mesInicial = 1
+var anoInicial = 2000
+
+var mesesDoAno = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out","Nov","Dez"]
+var diasDaSemana = ["Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"]
+
+primeiroDia = diasDaSemana[5]
+
+let objDia = { diaDaSemana: diaDaSemana, dia: dia }
+
+for (let i = anoInicial; i <= anoInicial + 50; i++) {
+    for(let l = mesInicial; l<=mesesDoAno.length;l++){
+        for(let k =diaInicial; k<=diasDaSemana.length;k++){
+            semana = {}
+        }
+    }
+
+    calendario.add
+            {
+                i: {
+                    
+                }
+            }
+}
